94b4ff949f2d98c0fd59d45751efd34f86e6e535
codebender14 <111784806+codebender14@users.noreply.github.com>
2022-11-02 16:30:12 -0500

    updated the app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var appRouter = require('./routes/Function');
+var appRouter = require('./routes/function');
 var gridbuildRouter = require('./routes/gridbuild');
 
 var app = express();
