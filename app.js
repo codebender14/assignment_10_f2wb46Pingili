422deb522c640009dfb3e7d5c0951c12749376cb
codebender14 <111784806+codebender14@users.noreply.github.com>
2022-11-02 16:30:40 -0500

    updated the app.js file

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Function', appRouter);
+app.use('/function', appRouter);
 app.use('/gridbuild', gridbuildRouter);
 
 // catch 404 and forward to error handler
